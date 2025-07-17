8efdc68d25e1ff9d356ee8ff41a6df3c613f1112
eesuhn <eason.yihong@gmail.com>
2025-07-17 23:52:19 +0800

    wip: Freeze NER base model

--- src/modeling/ner/model.py
+++ src/modeling/ner/model.py
@@ -11,11 +11,20 @@ REPORTS_MODEL_DIR = REPORTS_DIR / "model"
 
 @dataclass
 class NerModelConfig:
+    # Base model
     model_name: str = "tomaarsen/span-marker-roberta-large-ontonotes5"
     cache_dir = str(MODEL_DIR / model_name)
+
+    # Training params
+    freeze_base_model: bool = True
+    freeze_encoder: bool = True
+    freeze_classifier: bool = False  # NOTE: We don't need to freeze this for now
+
+    # Decision layer
     hidden_size: int = 512
     dropout: float = 0.1
 
+    # Labels
     base_labels: list = field(default_factory=list)
     new_labels: list = field(default_factory=list)
     all_labels: list = field(default_factory=list)
@@ -38,10 +47,11 @@ class NerModel(nn.Module):
         super().__init__(*args, **kwargs)
         self.nmc = NerModelConfig()
         self.base_model = self._init_ner_model()
-        self._freeze_base_model()
+        self._freeze_base_model(show_info=True)
 
-        # NerModelHelper._save_ner_base_model_config(self.base_model)
-        # NerModelHelper._save_ner_base_model_encoder_config(self.base_model)
+        if self.save_config:
+            NerModelHelper._save_ner_base_model_config(self.base_model)
+            NerModelHelper._save_ner_base_model_encoder_config(self.base_model)
 
     def _init_ner_model(self) -> SpanMarkerModel:
         try:
@@ -54,27 +64,40 @@ class NerModel(nn.Module):
         except Exception as e:
             raise RuntimeError(f"Failed to init {self.nmc.model_name}: {e}")
 
-    def _freeze_base_model(self) -> None:
-        justsdk.print_info(f"Freezing params: {self.nmc.model_name}")
-        for p in self.base_model.parameters():
-            p.requires_grad_(False)
+    def _freeze_base_model(self, show_info: bool = False) -> None:
+        if not self.nmc.freeze_base_model:
+            justsdk.print_info(f"Disabled freezing {self.nmc.model_name}")
+            return
 
-    def _get_ner_feature_projection(self) -> nn.Sequential:
-        try:
-            encoder_hidden_size = self.base_model.encoder.config.hidden_size
-            target_hidden_size = self.nmc.hidden_size
-            seq = nn.Sequential(
-                nn.Linear(encoder_hidden_size, target_hidden_size),
-                nn.ReLU(),
-                nn.Dropout(self.nmc.dropout),
-                nn.Linear(target_hidden_size, target_hidden_size // 2),
-                nn.ReLU(),
-                nn.Dropout(self.nmc.dropout),
+        if not self.nmc.freeze_encoder and not self.nmc.freeze_classifier:
+            justsdk.print_warning(f"Nothing to freeze for {self.nmc.model_name}")
+            return
+
+        total_params: int = 0
+        frozen_params: int = 0
+
+        justsdk.print_info(f"Freezing {self.nmc.model_name}")
+        for name, param in self.base_model.named_parameters():
+            total_params += param.numel()
+            should_freeze = False
+
+            if self.nmc.freeze_encoder and "encoder" in name:
+                should_freeze = True
+            elif self.nmc.freeze_classifier and "classifier" in name:
+                should_freeze = True
+
+            if should_freeze:
+                param.requires_grad_(False)
+                frozen_params += param.numel()
+
+        if show_info:
+            justsdk.print_info(
+                f"Frozen {frozen_params:,} / {total_params:,} "
+                f"({frozen_params / total_params:.2%})"
             )
-            justsdk.print_success("Created NER feature projection")
-            return seq
-        except Exception as e:
-            raise RuntimeError(f"Failed to get NER feature projection: {e}")
+
+        if frozen_params == 0:
+            justsdk.print_warning(f"Nothing frozen for {self.nmc.model_name}")
 
 
 class NerModelHelper:
@@ -87,7 +110,7 @@ class NerModelHelper:
             use_orjson=True,
             atomic=True,
         )
-        justsdk.print_info(f"Config written to: {output_path}")
+        justsdk.print_info(f"Config written to {output_path}")
 
     @staticmethod
     def _save_ner_base_model_encoder_config(base_model: SpanMarkerModel) -> None:
@@ -98,7 +121,7 @@ class NerModelHelper:
             use_orjson=True,
             atomic=True,
         )
-        justsdk.print_info(f"Config written to: {output_path}")
+        justsdk.print_info(f"Config written to {output_path}")
 
 
 if __name__ == "__main__":
